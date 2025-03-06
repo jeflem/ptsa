f74f4999e56e3919f450edeaaf69f32df18bebb7
Jens Flemming <jens.flemming@fh-zwickau.de>
2025-03-06 06:26:13 +0100

    Fix coloring of stops with comments/warnings for stop components

--- frontend/map.js
+++ frontend/map.js
@@ -299,8 +299,8 @@ function getStopColor(props) {
     }
     
     if (document.getElementById('warn').checked == true) {
-        var comm = props['member_comments'] == 'True';
-        var warn = props['member_warnings'] == 'True' || props['warnings'] != '';
+        var comm = props['member_comments'] == true;
+        var warn = props['member_warnings'] == true || props['warnings'] != '';
         if (warn == true && document.getElementById('warn_warn').checked == true) {
             return warnColors['warn'];
         } else if (comm == true && document.getElementById('warn_comm').checked == true) {
